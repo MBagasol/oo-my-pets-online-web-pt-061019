de289100d7f97d4ea5ebd25a1f6fbc6f60931b76
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-27 00:41:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,8 +45,8 @@ class Owner
   end
   
   def sell_pets
-    @dogs.collect {|dog| dog.mood = "nervous" dog.owner = nil}
-    @cats.collect {|cat| cat.mood = "nervous" cat.owner = nil}
+    @dogs.collect {|dog| dog.mood = "nervous" , dog.owner = nil}
+    @cats.collect {|cat| cat.mood = "nervous" , cat.owner = nil}
     #@dogs.collect {|dog| dog.owner = nil}
     #@cats.collect {|cat| cat.owner = nil}
   end
