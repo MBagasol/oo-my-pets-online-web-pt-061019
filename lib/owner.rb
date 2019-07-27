1f723a5fb01ef89d10a392e505ed50d8de11e52e
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-07-27 00:09:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  attr_reader :name , :species
+  attr_accessor :cats, :dogs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count 
+    self.all.length 
+  end
+  
+  def self.reset_all 
+    self.all.clear 
+  end
+  
+  
+    
+  
+  
 end
